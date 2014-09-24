ac3aa668e29cd197e0199220d09065b8461881b6
bencsont <ben_csont@yahoo.com>
2014-09-24 12:05:32 +0200

    Raderade allt. Skrev en enda sysout.

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,7 @@
 
 public class Test {
 	public static void main(String[] args) {
-		for(int i = 0; i < 3; i++)
-			System.out.println("Testar lite Git här...");
-		System.out.println("Testar lite Git här...");
-		System.out.println("Hej hopp.");
-		System.out.println("Testar också");
-		System.out.println("En till test");
 		
-		System.out.println("Test. Test. Test");
+		System.out.println("Deleted previous");
 	}
 }
