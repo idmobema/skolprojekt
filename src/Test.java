32825308e922ed8945b076ec341ef0cb1f69081d
Beni <ben_csont@yahoo.com>
2014-09-24 10:11:21 +0200

    yhfh

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,9 @@
 
 public class Test {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		for(int i = 0; i < 3; i++)
 			System.out.println("Testar lite Git här...");
-=======
 		System.out.println("Testar lite Git här...");
 		System.out.println("Hej hopp.");
->>>>>>> origin/master
 	}
 }
