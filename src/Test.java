4108b7705202758ff3c9373ac90439228cdc962a
Moa <moa@example.com>
2014-09-24 10:15:22 +0200

    hej

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Testar lite Git här...");
 		System.out.println("Hej hopp.");
+		System.out.println("Testar också");
 	}
 }
