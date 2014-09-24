b916be7adb45baee155272871203ec4557b5ecfd
Beni <ben_csont@yahoo.com>
2014-09-24 09:51:25 +0200

    ghdfg

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Testar lite Git här...");
+		for(int i = 0; i < 3; i++)
+			System.out.println("Testar lite Git här...");
 	}
 }
