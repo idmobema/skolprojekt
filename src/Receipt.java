4ab875f1b8b9f43120cdcc5d5a3ebd610e6f8b5f
Mats Rörbecker <mats@rorbecker.com>
2014-09-29 17:07:47 +0200

    Update Receipt + ReceiptTest

--- src/Receipt.java
+++ src/Receipt.java
@@ -1,4 +1,13 @@
 
 public class Receipt {
-
+	private static String delimiter;
+	
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+	
+	@Override
+	public String toString() {
+		return delimiter;
+	}
 }
--- test/ReceiptTests.java
+++ test/ReceiptTests.java
@@ -6,8 +6,25 @@ import org.junit.Test;
 public class ReceiptTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIfToStringReturnsEmptyString() {
+		Receipt r = new Receipt();
+		r.setDelimiter("");
+		assertEquals("", r.toString());
+	}
+	
+	@Test
+	public void testIfToStringReturnsSomething() {
+		Receipt r = new Receipt();
+		r.setDelimiter("----------------------------------------");
+		assertTrue(r.toString().length() != 0);
+	}
+	
+	@Test
+	public void testIfToStringReturnsSameString() {
+		Receipt r1 = new Receipt();
+		r1.setDelimiter("----------------------------------------");
+		Receipt r2 = new Receipt();
+		assertEquals(r1.toString(), r2.toString());
 	}
 
 }
