f1a077fcb5c08f6c1d650e5dea8f4727d9b1eb64
Beniamin <ben_csont@yahoo.com>
2014-10-02 11:19:05 +0200

    Started Discount class

--- src/Discount.java
+++ src/Discount.java
@@ -3,7 +3,34 @@ public class Discount {
 
 	private double percentage;
 	
+	public Discount(double percentage){
+		try{
+			checkPercentage(percentage);
+			this.percentage = percentage;
+		}
+		catch(IllegalArgumentException iae){
+			System.out.println("Percentage has to be between 0 and 75 inclusive.");
+		}
+		
+		
+	}
+
+	private void checkPercentage(double percentage) {
+		if(percentage < 0 || percentage > 75)
+			throw new IllegalArgumentException ();
+	}
+	
 	public double getPercentage(){
-		return 12.5;
+		return percentage;
+	}
+	
+	public void setPercentage(double percentage){
+		try{
+			checkPercentage(percentage);
+			this.percentage = percentage;
+		}
+		catch(IllegalArgumentException iae){
+			System.out.println("Percentage has to be between 0 and 75 inclusive.");
+		}
 	}
 }
--- test/DiscountTest.java
+++ test/DiscountTest.java
@@ -1,14 +1,34 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import org.junit.Before;
 
 public class DiscountTest {
 
+	Discount d1, d2, d3, d4, d5;
+	@Before
+	public void setUpDiscountObjects(){
+		d1 = new Discount(-1);
+		d2 = new Discount(0);
+		d3 = new Discount(12.34);
+		d4 = new Discount(75);
+		d5 = new Discount(76);
+	}
+	
 	@Test
 	public void testGetPercentage() {
-		Discount discount = new Discount();
-		assertTrue(discount.getPercentage() == 12.5);
+		assertTrue("should return 12.34", d3.getPercentage() == 12.34);
+	}
+	
+	@Test //(expected = IllegalArgumentException.class)
+	public void testCheckPercentage(){
+		assertFalse(d1.getPercentage() == -1);
+		assertFalse(d5.getPercentage() == 76);
+	}
+	
+	@Test
+	public void testSetProcentage(){
+		d3.setPercentage(20.40);
 	}
 
 }
