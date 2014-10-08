162b968e94b57af92e922100e6603ccbccdaccc0
Mats Rörbecker <mats@rorbecker.com>
2014-10-08 19:25:17 +0200

    Adds methods to Money class

--- src/Money.java
+++ src/Money.java
@@ -10,8 +10,27 @@ public class Money {
 		return amount;
 	}
 	
-	// Should return new Money object, objects used in addition should be unchanged
-	public Money add(Money addend) {
+	public Money plus(Money addend) {
 		return new Money(amount + addend.getAmount());
+	}
+	
+	public Money minus(Money subtrahend) {
+		if (subtrahend.getAmount() > amount)
+			throw new IllegalArgumentException("Subtrahend must not be greater than minuend.");
+		return new Money(amount - subtrahend.getAmount());
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Money) {
+			Money otherMoney = (Money) other;
+			return otherMoney.amount == amount;
+		}
+		return false;	
+	}
+
+	@Override
+	public int hashCode() {
+		return 1337 * amount;
 	}
 }
--- test/MoneyTests.java
+++ test/MoneyTests.java
@@ -20,9 +20,29 @@ public class MoneyTests {
 
 	@Test
 	public void testAdditionOfMoneyObjects() {
-		Money fifteen = five.add(ten);
+		Money fifteen = five.plus(ten);
 		assertEquals(15, fifteen.getAmount());
 		assertEquals(5, five.getAmount());
 		assertEquals(10, ten.getAmount());
+	}
+	
+	@Test 
+	public void testSubtractionOfMoneyObjects() {
+		Money otherFive = ten.minus(five);
+		assertEquals(otherFive.getAmount(), five.getAmount());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testSubtractionWithInvalidSubtrahend() {
+		five.minus(ten);
+	}
+	
+	@Test
+	public void testEquality() {
+		Money otherFive = new Money(5);
+		assertTrue(five.equals(otherFive));
+		assertEquals(five.hashCode(), otherFive.hashCode());
+		assertFalse(five.equals(ten));
+		assertNotEquals(five.hashCode(), ten.hashCode());
 	}
 }
