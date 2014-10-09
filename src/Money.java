bae93b65cb1f1cbc39306faf7ffc0bcf58a2604d
Mats Rörbecker <mats@rorbecker.com>
2014-10-09 20:17:27 +0200

    Adds argument checking, proper equality to Money class

--- src/Money.java
+++ src/Money.java
@@ -1,36 +1,51 @@
 
 public class Money {
 	private int amount;
+	private String currency;
 	
-	public Money(int amount) {
+	public Money(int amount, String currency) {
+		if (amount < 0)
+			throw new IllegalArgumentException("Invalid amount: " + amount + ". Value must not be negative.");
+		if (currency.length() != 3)
+			throw new IllegalArgumentException("Invalid currency acronym.");
 		this.amount = amount;
+		this.currency = currency;
 	}
 	
 	public int getAmount() {
 		return amount;
 	}
 	
+	public String getCurrency() {
+		return currency;
+	}
+	
 	public Money plus(Money addend) {
-		return new Money(amount + addend.getAmount());
+		return new Money(amount + addend.getAmount(), currency);
 	}
 	
 	public Money minus(Money subtrahend) {
 		if (subtrahend.getAmount() > amount)
-			throw new IllegalArgumentException("Subtrahend must not be greater than minuend.");
-		return new Money(amount - subtrahend.getAmount());
+			throw new IllegalArgumentException("Invalid subtrahend: " + subtrahend + ". Value must not be greater than minuend.");
+		return new Money(amount - subtrahend.getAmount(), currency);
 	}
 
 	@Override
 	public boolean equals(Object other) {
 		if (other instanceof Money) {
 			Money otherMoney = (Money) other;
-			return otherMoney.amount == amount;
+			return otherMoney.amount == amount && otherMoney.currency.equals(currency);
 		}
 		return false;	
 	}
 
 	@Override
 	public int hashCode() {
-		return 1337 * amount;
+		return amount * 1337 + 1337;
+	}
+	
+	@Override
+	public String toString() {
+		return "" + amount + " " + currency;
 	}
 }
--- test/MoneyTests.java
+++ test/MoneyTests.java
@@ -5,11 +5,12 @@ import org.junit.Before;
 
 public class MoneyTests {
 	private Money five, ten;
+	private final String SEK = "SEK";
 	
 	@Before
 	public void setUp() {
-		five = new Money(5);
-		ten = new Money(10);
+		five = new Money(5, SEK);
+		ten = new Money(10, SEK);
 	}
 	
 	@Test
@@ -39,10 +40,36 @@ public class MoneyTests {
 	
 	@Test
 	public void testEquality() {
-		Money otherFive = new Money(5);
+		Money otherFive = new Money(5, SEK);
 		assertTrue(five.equals(otherFive));
 		assertEquals(five.hashCode(), otherFive.hashCode());
 		assertFalse(five.equals(ten));
 		assertNotEquals(five.hashCode(), ten.hashCode());
+		Money tenDollar = new Money(10, "USD");
+		assertFalse(ten.equals(tenDollar));
+	}
+		
+	@Test
+	public void testZeroAmount() {
+		Money zero = new Money(0, SEK);
+		assertEquals(zero.getAmount(), 0);
+	}
+	
+	@Test
+	public void testGetCurrency() {
+		assertEquals(SEK, ten.getCurrency());
+		Money dollar = new Money(1, "USD");
+		assertEquals("USD", dollar.getCurrency());
+	}
+	
+	@Test
+	public void testIfToStringReturnsAmountAndCurrency() {
+		assertTrue(five.toString().contains("5"));
+		assertTrue(five.toString().contains(SEK));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidCurrencyName() {
+		new Money(20, "SE");
 	}
 }
