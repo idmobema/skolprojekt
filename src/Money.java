44a66950577c112908c9cbdae6d6ec0ed15dcf45
Mats Rörbecker <mats@rorbecker.com>
2014-10-14 09:52:26 +0200

    Added some more operations to Money class

--- src/Money.java
+++ src/Money.java
@@ -1,7 +1,7 @@
 import java.math.BigDecimal;
 import java.util.Currency;
 
-public class Money {
+public class Money implements Comparable<Money> {
 	// Konstant som anger förhållandet mellan den mindre och den större valutaenheten, t ex antalet ören per krona
 	public static int FRACTIONS = 100;
 	
@@ -9,16 +9,10 @@ public class Money {
 	private long amount;
 	private Currency currency;
 	
-	// Konstruktor som tar summan i ören (t ex) och ett Currency-objekt
-	public Money(long amount, Currency currency) {
-		if (amount < 0)
-			throw new IllegalArgumentException("Invalid amount: " + amount + ". Value must not be negative.");
-		this.amount = amount;
-		this.currency = currency;
-	}
-	
 	// Konstruktor som tar summan i ören (t ex) och en valutakod (t ex "SEK", "USD" etc)
 	public Money(long amount, String currencyCode) {
+		if (amount < 0)
+			throw new IllegalArgumentException("Invalid amount: " + amount + ". Value must not be negative.");
 		this.amount = amount;
 		try {
 			this.currency = Currency.getInstance(currencyCode);
@@ -27,17 +21,27 @@ public class Money {
 			throw new IllegalArgumentException("Invalid currency code: " + currencyCode);
 		}
 	}
-	
-	// Två konstruktorer som tar summan i den större valutaenheten, t ex kronor, och räknar om den till den mindre enheten
-	public Money(double amount, Currency currency) {
-		this(Math.round(FRACTIONS * amount), currency);
-	}
 
+	// Konstruktor som tar summan i ören (t ex) och ett Currency-objekt
+	public Money(long amount, Currency currency) {
+		this(amount, currency.getCurrencyCode());
+	}
+	
+	// Två motsvarande konstruktorer som tar summan i den större valutaenheten, t ex kronor, och räknar om den till den mindre enheten
 	public Money(double amount, String currencyCode) {
 		this(Math.round(FRACTIONS * amount), currencyCode);
 	}	
 	
-	// Summans get-metod returnerar en BigDecimal, för ökad precision
+	public Money(double amount, Currency currency) {
+		this(Math.round(FRACTIONS * amount), currency.getCurrencyCode());
+	}
+	
+	// "Hjälpkonstruktor" för att enkelt kunna skapa kronor
+	public static Money getCrowns(double amount) {
+		return new Money(amount, Currency.getInstance("SEK"));
+	}
+
+	// Summans get-metod returnerar en BigDecimal, för ökad precision och snygg formatering
 	public BigDecimal getAmount() {
 		return BigDecimal.valueOf(amount, currency.getDefaultFractionDigits());
 	}
@@ -53,16 +57,31 @@ public class Money {
 	
 	// Obs att plus inte förändrar det befintliga objektet, utan returnerar ett nytt objekt
 	public Money plus(Money addend) {
+		checkCurrency(addend);
 		return new Money(amount + addend.amount, getCurrencyCode());
 	}
 	
 	// Obs att minus inte förändrar det befintliga objektet, utan returnerar ett nytt objekt
 	public Money minus(Money subtrahend) {
+		checkCurrency(subtrahend);
 		if (subtrahend.amount > amount)
 			throw new IllegalArgumentException("Invalid subtrahend: " + subtrahend + ". Value must not be greater than minuend.");
 		return new Money(amount - subtrahend.amount, getCurrencyCode());
 	}
-
+	
+	// Hjälpmetod för kontroll av valuta vid aritmetiska operationer
+	private void checkCurrency(Money other) {
+		if (!other.currency.equals(currency))
+			throw new IllegalArgumentException("Invalid argument. Money in different currencies can not be added, subtracted or compared.");
+	}
+	
+	// Obs att times inte förändrar det befintliga objektet, utan returnerar ett nytt objekt
+	public Money times(int multiplier) {
+		if (multiplier < 0)
+			throw new IllegalArgumentException("Invalid multiplier: " + multiplier + ". Value must be zero or positive.");
+		return new Money(amount * multiplier, "SEK");
+	}
+	
 	@Override
 	public boolean equals(Object other) {
 		if (other instanceof Money) {
@@ -72,9 +91,15 @@ public class Money {
 		return false;	
 	}
 
+	// Ful-implementation som bör ändras vid tillfälle
 	@Override
 	public int hashCode() {
 		return (int) amount + 1337;
+	}
+	
+	public int compareTo(Money other) {
+		checkCurrency(other);
+		return (int) (amount - other.amount);
 	}
 	
 	@Override
--- test/MoneyTests.java
+++ test/MoneyTests.java
@@ -4,15 +4,18 @@ import org.junit.Test;
 import org.junit.Before;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 
 public class MoneyTests {
-	private Money five, ten;
+	private Money five, ten, zero, dollar;
 	private final String SEK = "SEK";
 	
 	@Before
 	public void setUp() {
 		five = new Money(500, SEK);
 		ten = new Money(1000, SEK);
+		zero = new Money(0, SEK);
+		dollar = new Money(1, "USD");
 	}
 	
 	@Test
@@ -20,7 +23,19 @@ public class MoneyTests {
 		assertEquals(new BigDecimal("5.00"), five.getAmount());
 		assertEquals(new BigDecimal("10.00"), ten.getAmount());
 	}
+	
+	@Test
+	public void testConstructorThatTakesDouble() {
+		Money fifteen = new Money(15.0, SEK);
+		assertEquals(new BigDecimal("15.00"), fifteen.getAmount());
+	}
 
+	@Test
+	public void testConstructorThatTakesCurrency() {
+		Money twenty = new Money(20.0, Currency.getInstance("SEK"));
+		assertEquals(new BigDecimal("20.00"), twenty.getAmount());
+	}
+	
 	@Test
 	public void testAdditionOfMoneyObjects() {
 		Money fifteen = five.plus(ten);
@@ -53,14 +68,12 @@ public class MoneyTests {
 		
 	@Test
 	public void testZeroAmount() {
-		Money zero = new Money(0, SEK);
 		assertEquals(zero.getAmount(), new BigDecimal("0.00"));
 	}
 	
 	@Test
 	public void testGetCurrency() {
 		assertEquals(SEK, ten.getCurrency().getCurrencyCode());
-		Money dollar = new Money(1, "USD");
 		assertEquals("USD", dollar.getCurrency().getCurrencyCode());
 	}
 	
@@ -73,5 +86,54 @@ public class MoneyTests {
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidCurrencyName() {
 		new Money(200, "SE");
+	}
+
+	@Test
+	public void testMultiplicationOfMoneyObjects() {
+		Money fifteen = new Money(15.0, SEK);
+		assertEquals(fifteen, five.times(3));
+		assertEquals(zero, five.times(0));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidMultiplier() {
+		five.times(-1);
+	}
+	
+	@Test
+	public void testRoundingOfDoubleAmount() {
+		Money testMoney = new Money(10.815, SEK);
+		assertEquals(new BigDecimal("10.82"), testMoney.getAmount());
+		testMoney = new Money(10.8149, SEK);
+		assertEquals(new BigDecimal("10.81"), testMoney.getAmount());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAdditionWithDifferentCurrencies() {
+		five.plus(dollar);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSubtractionWithDifferentCurrencies() {
+		five.minus(dollar);
+	}
+	
+	@Test
+	public void testHelperConstructorForCrowns() {
+		Money crown = Money.getCrowns(1.0);
+		assertEquals(SEK, crown.getCurrency().getCurrencyCode());
+	}
+	
+	@Test
+	public void testComparison() {
+		assertTrue(ten.compareTo(five) > 0);
+		assertTrue(five.compareTo(ten) < 0);
+		Money otherFive = new Money(5.0, SEK);
+		assertTrue(five.compareTo(otherFive) == 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testComparisonWithDifferentCurrencies() {
+		five.compareTo(dollar);
 	}
 }
