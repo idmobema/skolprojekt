1875a27e75b1400d8b14a181cc3c55e6129b35db
Ida Östlund <idas_reserv@hotmail.se>
2014-10-15 10:58:49 +0200

    Fortsatt på klasserna

--- test/AddressTester.java
+++ test/AddressTester.java
@@ -3,6 +3,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
+//Mellanslag i postnummer
+//Postnummer får ej börja på 0
+//Postnummer 3+2 siffror
+//Allt med Postort samt koppling mellan postnummer och postort
+
 public class AddressTester {
 
 	@Test
@@ -16,15 +21,75 @@ public class AddressTester {
 		Address addr = new Address("", "1", "22334", "Platsen");
 	}
 	
+	
+	
 	@Test
 	public void testStreetNumberIsSaved(){
 		Address addr = new Address("Gatan", "2", "33445", "Platsen");
 		assertEquals("2", addr.getStreetNumber());
 	}
 	
-	@Test
-	public void testStreetNumberIsIntegerApartFrom(){
-		
+	@Test(expected = NumberFormatException.class)
+	public void testNoNumberInStreetNumber(){
+		Address addr = new Address("Stigen", "ett", "44556", "Staden");
 	}
+	
+	@Test
+	public void testStreetNumberWithLetter(){
+		Address addr = new Address("Allén", "1a", "55667", "Byn");
+		assertEquals("1a", addr.getStreetNumber());
+	}
+	
+	@Test
+	public void testStreetNumberWithSpaceBetweenNumberAndLetter(){
+		Address addr = new Address("Kroken", "11 b", "66778", "Samhället");
+		assertEquals("11 b", addr.getStreetNumber());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testStreetNumberIsZero(){
+		Address addr = new Address("Svängen", "0", "88990", "Storstaden");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNegativeStreetNumber(){
+		Address addr = new Address("Min väg", "-4", "99001", "Småstaden");
+	}
+	
+	
+	
+	
+	
+	@Test
+	public void testPostalCodeIsSaved(){
+		Address addr = new Address("Ön", "3", "77889", "Stället");
+		assertEquals("77889", addr.getPostalCode());
+	}
+	
+	@Test (expected = NumberFormatException.class)
+	public void testPostalCodeNotANumber(){
+		Address addr = new Address("Ån", "4", "123c4", "Hemorten");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPostalCodeStartingWithZero(){
+		Address addr = new Address("Slingan", "45", "02344", "Stockholm");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testTooShortPostalCode(){
+		Address addr = new Address("Din väg", "12", "1122", "Kista");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testTooLongPostalCode(){
+		Address addr = new Address("Mörtgatan", "15", "112233", "Fisksätra");
+	}
+	
+	
+	
+	
+	
+	
 
 }
