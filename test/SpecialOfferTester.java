3430c11ed394d5a6f6bef46b07a059f58068ccfe
Mats Rörbecker <mats@rorbecker.com>
2014-10-22 10:29:10 +0200

    Added tests for SpecialOffer for full coverage

--- test/SpecialOfferTester.java
+++ test/SpecialOfferTester.java
@@ -173,5 +173,13 @@ public class SpecialOfferTester {
 		assertEquals(sO1, sO2);
 	}
 	
-	
+	@Test
+	public void testHashCode(){
+		Discount sO1 = new SpecialOffer("TwoForThePriceOfOne", 2015, 4, 20, 2015, 4, 25);
+		Discount sO2 = new SpecialOffer("TwoForThePriceOfOne", 2015, 4, 20, 2015, 4, 25);
+		System.out.println(sO1.hashCode());
+		System.out.println(sO2.hashCode());
+		
+		assertTrue(sO1.hashCode() == sO2.hashCode());		
+	}
 }
